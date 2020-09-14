6e6299a0da3386e87485bb990f077e08ef57c768
JaswanthreddyPadala <jaswanthreddypadala@gmail.com>
2020-09-14 15:48:00 +0530

    Updates

---
+++ precache-manifest.a9b88ccb6bdcceba6dc4e5d737c602a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfe165bb2289a0cf96bba37fc3b7384",
+    "url": "/https.//JaswanthReddyPadala.github.io/React-Course---Monster-rolodex/index.html"
+  },
+  {
+    "revision": "e901e0f3737a40ceb1d1",
+    "url": "/https.//JaswanthReddyPadala.github.io/React-Course---Monster-rolodex/static/css/main.f7583dd2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/https.//JaswanthReddyPadala.github.io/React-Course---Monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https.//JaswanthReddyPadala.github.io/React-Course---Monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e901e0f3737a40ceb1d1",
+    "url": "/https.//JaswanthReddyPadala.github.io/React-Course---Monster-rolodex/static/js/main.2790d82d.chunk.js"
+  },
+  {
+    "revision": "8ed5d9f57ae6a8732ab1",
+    "url": "/https.//JaswanthReddyPadala.github.io/React-Course---Monster-rolodex/static/js/runtime-main.4ef1c620.js"
+  }
+]);
